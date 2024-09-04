3679b82ab83abd7442df02f442796024a65f393c
TrustTroyy <jwinston10@mail.valenciacollege.edu>
2024-09-04 16:30:20 -0700

    Add files via upload
    Upload of VariableFun

---
+++ VariableAssignment.py
@@ -0,0 +1,12 @@
+multiplication_result = 5 * 6
+print("30")
+division_result = 8 / 3
+print(2.66666667)
+division_without_remainder = 8//3
+print(2)
+modulo_result = 8 % 3
+print(2)
+addition_result = 4200000 + 23090
+print(4223090)
+subtraction_result = 4200000 - 7633
+print(4192367)
